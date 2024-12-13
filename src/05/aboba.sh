39af7f28f49b9703645e73800c8077d3cbd7df2f
home <asskkord@gmail.com>
2024-12-13 23:03:28 +0300

    finish

--- src/05/aboba.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-counter=1
-du ~/projects/ | sort -n -r | head -n 5 | while read line; do
-    printf "%d - %s, %.0f MB\n" $counter $(echo "$line" | awk '{printf $2}') $(echo "$line" | awk '{printf $1/1000}')
-    counter=$(( counter + 1 ))
-    #echo "$line"
-done
--- src/05/main.sh
+++ src/05/main.sh
@@ -1,23 +1,54 @@
 #!/bin/bash
 
-printf "Total number of folders (including all nested ones) = %s\n" "$(( $(find $1 -type d | wc -l) - 1 ))"
+start=$(date +%s)
 
-printf "TOP 5 folders of maximum size arranged in descending order (path and size):\n"
+if [[ $# = 1 ]]
+then
 
-counter=1
-du $1 | sort -n -r | head -n 5 | while read line; do
-    printf "%d - %s, %.0f MB\n" $counter $(echo "$line" | awk '{printf $2}') $(echo "$line" | awk '{printf $1/1000}')
-    counter=$(( counter + 1 ))
-done
+    if [[ $1 == */ ]]
+    then
 
-printf "Number of:\n"
+        printf "Total number of folders (including all nested ones) = %s\n" "$(( $(find $1 -type d | wc -l) - 1 ))"
 
-printf "Configuration files (with the .conf extension) = %s\n" "$(find $1 -name *.conf | wc -l)"
+        printf "TOP 5 folders of maximum size arranged in descending order (path and size):\n"
+        counter_dirs=1
+        du $1 | sort -n -r | head -5 | while read line; do
+            if [[ $1 == . ]]; then continue; fi
+            printf "%d - %s, %.0f MB\n" $counter_dirs $(echo "$line" | awk '{printf $2}') $(echo "$line" | awk '{printf $1/1000}')
+            counter_dirs=$(( counter_dirs + 1 ))
+        done
 
-printf "Text files = %s\n" "$(find $1 -type f -exec file {} \; | awk '{$1="";print $0}' | awk '/UTF|ASCII|Unicode/' | wc -l)"
+        printf "Number of:\n"
 
-printf "Executable files = %s\n" "$(find $1 -type f -exec file {} \; | awk '{$1="";print $0}' | awk '/executable|script/' | wc -l)"
+        printf "Configuration files (with the .conf extension) = %s\n" "$(find $1 -name *.conf | wc -l)"
 
-printf "Log files (with the extension .log) = %s\n" "$(find $1 -name *.log | wc -l)"
+        printf "Text files = %s\n" "$(find $1 -type f -exec file {} \; | awk '{$1="";print $0}' | awk '/UTF|ASCII|Unicode/' | wc -l)"
 
-printf "Archive files = %s\n" "$(find $1 -type f -exec file {} \; | awk '{$1="";print $0}' | awk '/archive|tar|zip|gzip/' | wc -l)"
+        printf "Executable files = %s\n" "$(find $1 -type f -exec file {} \; | awk '{$1="";print $0}' | awk '/executable|script/' | wc -l)"
+
+        printf "Log files (with the extension .log) = %s\n" "$(find $1 -name *.log | wc -l)"
+
+        printf "Archive files = %s\n" "$(find $1 -type f -exec file {} \; | awk '{$1="";print $0}' | awk '/archive|tar|zip|gzip/' | wc -l)"
+
+        printf "Symbolic links = %s\n" "$(find $1 -type l| wc -l)"
+
+        printf "TOP 10 files of maximum size arranged in descending order (path, size and type):\n"
+        counter_files=1
+        find $1 -type f -printf "%s\t%p\n" | sort -nr | head -10 | while read line; do
+            printf "%d - %s, %.0f MB," $counter_files $(echo "$line" | awk '{printf $2}') $(echo "$line" | awk '{printf $1/1000}')
+            counter_files=$(( counter_files + 1 ))
+            printf "%s\n" "$( file $(echo "$line" | awk '{printf $2}') | awk '{$1="";print $0}')"
+        done
+
+        printf "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file):\n"
+        echo "$(find $1 -type f -executable -exec du -h {} + | sort -hr | head -10 | cat -n | awk '{print $1" - "$3", "$2}')"
+
+        end=$(date +%s)
+        printf "Script execution time (in seconds) = %s\n" $(( end - start ))
+
+    else
+        echo "Incorrect input"
+    fi
+else 
+    echo "Incorrect number of arguments"
+fi
